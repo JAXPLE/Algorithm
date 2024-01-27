a5daa3f6e4ea89d97a768a4e4af81853a0e9f364
jaxple <jaxple@gmail.com>
2024-01-27 11:35:04 +0900

    24/1/27 Algorithm

--- src/알고리즘/조합/Permutation.cpp
+++ src/알고리즘/조합/Permutation.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     // int arr[] = {1,2,3};
     vector<int> arr = {1,2,3};
-    // sort(array.begin(),arr.end());
+    sort(arr.begin(), arr.end());
     do {
         for (int i : arr)
             cout << i << " ";
