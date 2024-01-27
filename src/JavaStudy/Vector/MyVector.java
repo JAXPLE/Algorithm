5849b01c256e27348fdf86a6d08851351df6bb26
jaxple <jaxple@gmail.com>
2024-01-27 11:35:26 +0900

    24/1/27 Add Vector

---
+++ src/JavaStudy/Vector/MyVector.java
@@ -0,0 +1,14 @@
+package JavaStudy.Vector;
+
+class Vector {
+
+    public Vector() {
+
+    }
+}
+
+public class MyVector {
+    public static void main(String[] args) {
+        java.util.Vector<String> vector = new java.util.Vector<>();
+    }
+}
