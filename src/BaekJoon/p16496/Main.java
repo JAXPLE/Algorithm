67de674bbedd603df41f8b9ca1aaef5b11a5fb29
JAXPLE <jaxple@gmail.com>
2024-10-01 10:03:02 +0900

    Add: Split Code

---
+++ src/BaekJoon/p16496/Main.java
@@ -0,0 +1,12 @@
+package BaekJoon.p16496;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+        String[] strings = br.readLine().split(" ");  
+    }
+}
